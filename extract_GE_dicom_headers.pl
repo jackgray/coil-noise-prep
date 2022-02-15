616ae8ba5016c13a61b5b30a78616293bd06558e
Jack Gray <jgrayau@gmail.com>
2022-02-15 15:43:40 -0500

    update outputs on perl script to extract header info

--- extract_GE_dicom_headers.pl
+++ extract_GE_dicom_headers.pl
@@ -1,4 +1,4 @@
-#!/bin/perl
+#!/bin/env perl
 
 use strict;
 use warnings;
@@ -10,7 +10,27 @@ my @pathExtractArr = split(' ', $pathExtract);
 my $dicomPath = $pathExtractArr[2];
 print "$dicomPath";
 
+# Call GE scanner function "dumpDicomHeader" and store output to tmp file
 my $headerDump = `/export/home/mx/host/bin/dumpDicomHeader $dicomPath`;
-print "$headerDump";
+my $dumpFile = '/tmp/header_dump.txt'
+open(FH, '>', $dumpFile) or die $!;
+print FH $headerDump;
+close(FH);
 
-my $testField = `-lane 'print $F[$j-1] if $. == $i' -- -i=5 -j=3 sample_data.txt`
+# parse header dump by [i'\t'],[j'\n']
+my $exam = `-lane 'print $F[5] if $. == 62' $dumpFile`;
+my $project = `-lane 'print $F[5] if $. == 28' $dumpFile`;
+my $scanning_protocol = `-lane 'print $F[5] if $. == 89' $dumpFile`;
+
+my $series_desc = `-lane 'print $F[5] if $. == 29' $dumpFile`;
+my $pulse_seq_name = `-lane 'print $F[5] if $. == 134' $dumpFile`;
+
+my $subj_id = `-lane 'print $F[5] if $. == 61' $dumpFile`;
+my $acqDate = `-lane 'print $F[5] if $. == 16' $dumpFile`;
+my $subj_dob = `-lane 'print $F[5] if $. == 63' $dumpFile`;
+my $subj_age = `-lane 'print $F[5] if $. == 65' $dumpFile`;
+my $subj_gender = `-lane 'print $F[5] if $. == 64' $dumpFile`;
+my $subj_weight = `-lane 'print $F[5] if $. == 66' $dumpFile`;
+my $sequence_params = `-lane 'print $F[5] if $. == 71' $dumpFile`;
+my $total_imgs = `-lane 'print $F[5] if $. == 205' $dumpFile`;
+
