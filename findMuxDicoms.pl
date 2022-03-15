8c7a8507349ea023a6d67f962b87b40e7fb8775a
Jack Gray <jgrayau@gmail.com>
2022-03-15 09:24:25 -0400

    re-write to use legacy built-in only perl

--- findMuxDicoms.pl
+++ findMuxDicoms.pl
@@ -8,156 +8,135 @@ $currentExamFile = 'current_exam.txt';
 open(FH, '<', $currentExamFile) or die $!;
 $currentExam = int(<FH>);
 close(FH);
-print "Starting QC with exam $currentExam\n";
+print "\nStarting QC prep with exam $currentExam\n";
 
 # INITIALIZATION
-# $currentExam = 21400;
-$exam_tries = 3 + $currentExam;
 $currentSeries = 5;
 $currentImage = 1;
-$series_tries = 10 + $currentSeries;
 $isMux = 'false';
+
+$exam_ceiling = 3 + $currentExam;
+$series_ceiling = 10 + $currentSeries;
+$max_age = 1; # don't QC exams more than n days old
+
+$pathExtract = "/export/home/service/cclass/pathExtract $currentExam $currentSeries $currentImage";
+$dumpDicomHeader = "/export/home/mx/host/bin/dumpDicomHeader $dicomPath";
+$dicmCompParser = "/export/home/sdc/bin/dicmCompParser -i $dicomPath";
 $dateCommand = "date +%Y%m%d";
 $date = `$dateCommand`;
-print "Today's date: $date\n";
-$max_age = 1; # don't QC exams more than 2 days old
-print "starting loop";
+print "\nToday's date: $date\n";
 
+print "Starting loop to search for a recent MUX sequence.";
 while ($isMux eq 'false') {
     print "\n\n\n\n----------------------------------------------------";
-    $pathExtract = "/export/home/service/cclass/pathExtract $currentExam $currentSeries $currentImage";
     print "\nChecking exam $currentExam series $currentSeries ...\n";
-    $extractedPath = `$pathExtract`;
+    $extractedPath = `$pathExtract`;    # Returns path with fluff
     my @extractedArr = split(' ', $extractedPath);
-    #print "@extractedArr";
-    $dicomPath = $extractedArr[2];
+    $dicomPath = $extractedArr[2];      # Path without fluff
     print "Checking path:\n$dicomPath\n";
-    $dirnameCommand = "dirname $dicomPath";
     $dicomParent = `dirname $dicomPath`;
-    # $dicomParent = chomp($dicomParent);
-    print "Using parent path:\n$dicomParent\n";
-    $dumpDicomHeader = "/export/home/mx/host/bin/dumpDicomHeader $dicomPath";
-    $dicmCompParser = "/export/home/sdc/bin/dicmCompParser -i $dicomPath";
-    $dumpFile = "header_dump.txt";
+    print "\nUsing parent path:\n$dicomParent\n";
+    print "\nRetreiving header dump"
     $headerDump = `$dicmCompParser`;  # call GE shell command 'dicmCompParser'
-    # print $headerDump;
 
+    # Remove leading/trailing whitespaces -- doesn't work on GE system 
+    sub trim($) {
+        my $string = shift;
+        $string =~ s/^\s+//;
+        $string =~ s/\s+$//;
+        return $string;
+    }
 
-    @headers = split('#', $headerDump);
+    # PARSE HEADER DUMP / CREATE HASH TABLE 
+    @headers = split('#', $headerDump);    # Fieldnames start with #
     my %hash;
     foreach my $pair (@headers) {
-        # print "Adding $pair to hash\n";
         ($key, $val) = split('\n', $pair);
-        @valLine = split(' ', $val);
-        $value = $valLine[-1];
-        $key =~ s/^\s+//;
-        $key =~ s/\s+$//;
-        $value =~ s/^\s+//;
-        $value =~ s/\s+$//;
-        $value =~ tr/[]//dr;
-        # $value = $valEl =~ s/^\s+|\s+$//g;
-        print "Key: $key\n";
-        print "Value: $value\n";
-        # $headers{ $key } = $value;
-        $hash{$key} = $value;
+        @valLine = split(' ', $val);    # \t+ doesn't play nice here
+        $value = $valLine[3];   # human-readable value is 3rd column
+        $value =~ y/[]//d;      # Regex to remove brackets
+        $hash{$key} = $value;       # Push k:v to hash table
     }
-    print "$hash{'Study Description'}";
+    $exam_no = $hash{' Patient ID'};
+    $project = $hash{' Study Description'};
+    $protocol = $hash{' Study Name'};
+    $seq_name = $hash{' Pulse Sequence Name'};
+    $series_desc = $hash{' Series Description'};
+    $subj_id = $hash{" Patient's Name"};
+    $acq_date = $hash{' Acquisition Date'};
+    $series_no = $hash{" Series Number"};
+    $sequence_params = $hash{' Scan Options'};
 
+    # PRINT OUTPUT 
+    print "``````````````````````HEADER OUTPUT````````````````````````"
+    print "\nProject: $project";
+    print "\nProtocol: $protocol";
+    print "\nExam : $exam_no";
+    print "\nSeries: $series_no";
+    print "\nSeries Description: $series_desc";
+    print "\nDate Acquired: $acq_date";
+    print "\nSequence Parameters: $sequence_params\n";
+    print "```````````````````````````````````````````````````````````"
 
-    # $fuu = $hash{'Study Description'} ;
-    # print "$fuu";
-    # $lineOne = $headerLines[20];
-    # print "FFFF: $lineOne";
-    # $reg = ( $headerDump =~ m/\#([\s\S]*)\(/g );
-
-    # $headerLabels = split('(', $headerLines)
-    # $item = $headerLines[9];
-    # print "\n\n\n\nmatched: $item";
-    # print for @headerLines;
+    # Ensure exam is recent
+    if ( $acq_date < $date - $max_age ) {
+        if ( $acq_date == 0 ) {
+            print "\nExam number doesn't exist yet. There may not be new exams with MUX files. Check back tomorrow.";
+            exit 0;
+	    }
+	    print "\nCurrent exam $currentExam collected on $acq_date is too old.\nIncrementing exam number.";
+        $currentExam++;     # Check next exam until it is recent
+        next;
+    } elsif (index($sequence_params, 'HYPERBAND') != -1) {
+        print "\nSeries $series_no from exam $exam_no is a MUX file. Sending for cnqa.";
+        $isMux = 'true';
+        $currentExam++;	# increment for next QC check
+        last;   # Exit loop if MUX file
+    # Try next series for n tries
+    } elsif ($currentSeries < $series_ceiling) {
+        $currentSeries++;
+        print "\nThis series is not multiband. Checking series $currentSeries";
+        next;
+    } elsif ($currentExam == $exam_ceiling) {
+        print "\nExceeded retries looking for exam with MUX files. None found. Exiting";
+        exit 0;
+    } elsif ($currentSeries == $series_ceiling) {        
+        print "\nExceeded max tries for exam $currentExam. Checking next exam.";
+        $currentExam++;
+        $currentSeries = 6;
+	    $series_ceiling = $currentSeries + 5;
+        next;
+    # Explicitly exit loop on every condition to avoid infinite loop
+    } else {
+        print "Could not locate MUX file. Exiting.";
+	exit 0;
+    }
 }
-#     # WRITE TO FILE
-#     open(FH, '>', $dumpFile) or die $!;
-#     print FH $headerDump;
-#     close(FH);
 
-#     # PARSE HEADER
-#     my @dumpArr;
-#     open (FH, '<', $dumpFile) or die $!;
-#     while (<FH>) {
-#         #print "row: $_";
-#         chomp;
-#         my @row = split("\t+");
-#         print "$row[1]"
-#         push @dumpArr, $row[1];
-#     }
-#     close(FH);
+# FOUND MUX FILE
+print "\n\nFound recent MUX series to quality check at $dicomParent \
+\nAdding path to queue for compute server to pick up.";
 
-    # our $exam_no = $headers{'Study Desc'};
-    our $project = $headers{'Study Description'};
-#     our $protocol = $dumpArr[88];
-#     our $seq_name = $dumpArr[132];
-#     our $series_desc = $dumpArr[28];
-#     our $subj_id = $dumpArr[59];
-#     our $acq_date = $dumpArr[14];
-#     our $series_no = $dumpArr[195];
-#     our $sequence_params = $dumpArr[70];
+# Ensure single line
+chomp($dicomParent);
+my $src = catfile($dicomParent, "*");
+print "\nSource path:$src\n";
+$exam_no =~ s/^\s+|\s+$//g;
+my $dst = "/MRI_DATA/coil-noise/scans/${exam_no}_0${series_no}_${acq_date}";
+print "Destination path: $dst\n";
 
-    # print "\nProject: $project";
-#     print "\nExam : $exam_no";
-#     print "\nSeries: $series_no";
-#     print "\nSeries Description: $series_desc";
-#     print "\nDate Acquired: $acq_date";
-#     print "\nSequence Parameters: $sequence_params\n";
+# Write paths to first 100 dicoms to queue file
+@allPaths = glob($src)
+@nPaths = @allPaths[0..99];
+$queueFile = "qc_queue.txt";
+$destination = "qc_dst.txt";
+open(FH, '>', $queueFile) or die;
+    foreach @nPaths {
+        print FH "console:$_";
+    }
+close(FH);
 
-#     if ( $acq_date < $date - $max_age ) {
-#         if ( $acq_date == 0 ) {
-#             print "Exam number doesn't exist yet. There may not be new exams with MUX files. Check back tomorrow.";
-#             exit 0;
-# 	}
-# 	print "Current exam $currentExam collected on $acq_date is too old.\nIncrementing exam number.";
-#         $currentExam++;
-#         $currentSeries = 6;
-#         next;
-#     } elsif (index($sequence_params, 'HYPERBAND') != -1) {
-#         print "\nSeries $series_no from exam $exam_no is a MUX file. Sending for cnqa.";
-#         my $isMux = 'true';
-#         $currentExam++;	# increment for next QC check
-#         last;
-#     } elsif ($currentSeries < $series_tries) {
-#         $currentSeries++;
-#         print "This series is not multiband. Checking series $currentSeries";
-#         next;
-#     } elsif ($currentExam == $exam_tries) {
-#         print "Exceeded retries looking for exam with MUX files. None found. Exiting";
-#         exit 0;
-#     } elsif ($currentSeries == $series_tries) {        
-#         print "Exceeded max tries for exam $currentExam. Checking next exam.";
-#         $currentExam++;
-#         $currentSeries = 6;
-# 	$series_tries = $currentSeries + 5;
-#         next;
-#     } else {
-#         print "Could not locate MUX file. Exiting.";
-# 	exit 0;
-#     }
-# }
-
-# print "\n\nFound recent MUX series to quality check at $dicomParent Adding path to queue for compute server to pick up.";
-
-# chomp($dicomParent);
-# my $src = catfile($dicomParent, "*");
-# print "\nSource path:$src\n";
-# $exam_no =~ s/^\s+|\s+$//g;
-# my $dst = "/MRI_DATA/coil-noise/scans/${exam_no}_0${series_no}_${acq_date}";
-# print "Destination path: $dst\n";
-
-# # Write paths to queue file
-# my $queueFile = "qc_queue.txt";
-# open(FH, '>', $queueFile) or die;
-# print FH "$src $dst";
-# close(FH);
-
-# # Write updated exam number for next QC
-# open(FH, '>', $currentExamFile);
-# print FH $currentExam;
-# close(FH);
+# Write updated exam number for next QC
+open(FH, '>', $currentExamFile);
+print FH $currentExam;
+close(FH);
--- pull_qaScans.sh
+++ pull_qaScans.sh
@@ -1,19 +1,22 @@
 #! /bin/env bash 
 
-basePath=/MRI_DATA/coil-noise
+cnBase=/MRI_DATA/coil-noise
+# Copy source files queue defined on scanning console by prep script
+rsync -havzpPrti --itemize-changes --stats -e 'ssh -o StrictHostKeyChecking=no' console:~/scripts/qc_queue.txt ${cnBase}/pipelines >> ./pullQA.log
+# Copy destination path defined on scanning console by prep script
+rsync -havzpPrti --itemize-changes --stats -e 'ssh -o StrictHostKeyChecking=no' console:~/scripts/qc_queue.txt ${cnBase}/pipelines >> ./pullQA.log
 
-scp sdc@156.111.80.20:~/scripts/qc_queue.txt ${basePath}/pipelines
-
-src=`cat ${basePath}/pipelines/qc_queue.txt | cut -d' ' -f1`
-dst=`cat ${basePath}/pipelines/qc_queue.txt | cut -d' ' -f2`
+src=`cat ${basePath}/pipelines/qc_queue.txt`
+srcFile=${cnBase}/pipelines/qc_queue.txt
+dst=`cat ${basePath}/pipelines/qc_dst.txt`
 echo "Moving DICOMs from source: ${src} to destination ${dst}"
 
 # Create parent dir for data
 #dstParent=$(dirname ${dst})     # grab parent path of dicom files
 mkdir -p $dst             
 
-# Copy the data
-scp sdc@156.111.80.20:$src $dst
+# Copy the data- read src paths from file list generated on console
+rsync -havzpPrti --itemize-changes --stats -e 'ssh -o StrictHostKeyChecking=no' --files-from=${srcFile} ${dst} >> ./pullQA.log
 
 # Run cqna on copied data and redirect output to logfile
 ${basePath}/pipelines/cnqa $dst 2>&1 $basePath/auto_cnqa.log
